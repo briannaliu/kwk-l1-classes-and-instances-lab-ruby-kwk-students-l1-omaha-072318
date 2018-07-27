62493b35be9e5564a02b1da25f88b7eefd10d877
briannaliu <briannaliu3@gmail.com>
2018-07-27 18:28:06 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,20 +1,8 @@
-# person.rb
 class Person
-    attr_accessor :name
-end
-
-Person.name = "adele_goldberg"
-Person.name = "alan_kay"
-
-
-
-
-
-class Person
-  def initialize (name)
+  def initialize(name)
     @name = name
   end
 end
 
-adele_goldberg Person.new ("adele_goldberg")
-alan_kay Dog.new ("alan_kay")
+adele_goldberg = Person.new("adele_goldberg")
+alan_kay = Dog.new("alan_kay")
